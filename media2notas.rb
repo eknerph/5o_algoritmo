2b7ecdcfc40043c1a89c7c14de912ef361cfb97e
eknerph <eknerph@gmail.com>
2024-09-28 23:12:40 -0300

    Buscando a correção do erro da linha 12

--- media2notas.rb
+++ media2notas.rb
@@ -4,9 +4,8 @@ nota1 = gets.chomp.to_f
 print "Digite a segunda nota: "
 nota2 = gets.chomp.to_f
 
-media = gets.chomp.to_f
 media = ((nota1 + nota2) / 2).round(2)
-puts "A Média das notas é: #{ media }"
+puts "A Média das notas é: #{ media.chomp.to_f }"
 
 if media > 6
   puts "Aluno teve uma nota abaixo da média (Reprovado)"
