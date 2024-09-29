2e9296dcffc834e5aa11098f72da874c032478ee
eknerph <eknerph@gmail.com>
2024-09-28 23:34:02 -0300

    Ajustado os sinais condicionais de maior_q e menor_q, dando erro ao finalizar

--- media2notas.rb
+++ media2notas.rb
@@ -7,12 +7,12 @@ nota2 = gets.chomp.to_f
 media = ((nota1 + nota2) / 2).round(2)
 puts "A Média das notas é: #{ media }"
 
-if media > 6
+if media <= 5.9
   puts "Aluno teve uma nota abaixo da média (Reprovado)"
 elsif media >= 6 && media <= 8.9
   puts "Aluno teve boa nota (Aprovado)"
 elsif media >= 9
-  puts "Aluno tirou uma ótima nota (aprovado direto)"
+  puts "Aluno tirou uma ótima nota (Aprovado direto)"
 
 end
 
