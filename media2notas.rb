337a2049e11baf0bd88e839a23278f9416a1ab59
eknerph <eknerph@gmail.com>
2024-09-28 23:07:14 -0300

    Implemento o aprendisado de condicionais

--- media2notas.rb
+++ media2notas.rb
@@ -4,10 +4,15 @@ nota1 = gets.chomp.to_f
 print "Digite a segunda nota: "
 nota2 = gets.chomp.to_f
 
+media = gets.chomp.to_f
 media = ((nota1 + nota2) / 2).round(2)
 puts "A Média das notas é: #{ media }"
 
 if media > 6
+  puts "Aluno teve uma nota abaixo da média (Reprovado)"
+elsif media >= 6 && <= 8.9
+  puts "Aluno teve boa nota (Aprovado)"
+else media >= 9
 
 
 
