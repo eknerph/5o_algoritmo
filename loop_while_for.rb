edd2abb8f2fe5a725eec3b75e48775c32125e548
eknerph <eknerph@gmail.com>
2024-09-29 22:36:39 -0300

    Adicionado 1 código com exemplo de while

--- loop_while_for.rb
+++ loop_while_for.rb
@@ -9,30 +9,14 @@
 #   puts "Contador: #{ i }"
 # end
 
-# caixas_totais = 10
-# caixas_empilhadas = 0
+caixas_totais = 10
+caixas_empilhadas = 0
 
-# while caixas_empilhadas < caixas_totais
-#   puts "João empilha 2 caixas..."
-#   caixas_empilhadas += 2
-#   puts "João já empilhou #{ caixas_empilhadas } caixas."
-# end
-# puts " João terminou o trabalho e economizou tempo empilhando 2 caixas de cada vez!"
-
-
-puts "João vai empilhar 10 caixas."
-
-for caixa in 1..10
-  puts "João empilha a caixa número #{ caixa }"
+while caixas_empilhadas < caixas_totais
+  puts "João empilha 2 caixas..."
+  caixas_empilhadas += 2
+  puts "João já empilhou #{ caixas_empilhadas } caixas."
 end
-puts "João terminou de empilhar as caixas!\n\n"
+puts " João terminou o trabalho e economizou tempo empilhando 2 caixas de cada vez!"
 
-tijolos_totais = 1000
-tijolos_empilhados = 0 
 
-puts "João vai empilhar 1000 tijolos, sendo 50 tijolos por vez."
-
-while tijolos_empilhados < tijolos_totais
-  puts "João empilha 50 tijolos..."
-  tijolos_empilhados += 50
-  puts "João já empilhou #{ tijolos_empilhados } tijolos."
