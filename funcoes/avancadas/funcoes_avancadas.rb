f79001857d39ece89c3df68283b5e4dd06b2ebf9
eknerph <eknerph@gmail.com>
2024-09-30 22:11:15 -0300

    Adicionado a 1ª parte do 2º exemplo de função avançada.

--- funcoes/avancadas/funcoes_avancadas.rb
+++ funcoes/avancadas/funcoes_avancadas.rb
@@ -1,17 +1,35 @@
 # // Funções avançadas 01
-def process_numbers(numbers)
-  even_numbers = numbers.select do |num|
-    num.even?
-  end
+# def process_numbers(numbers)
+#   even_numbers = numbers.select do |num|
+#     num.even?
+#   end
 
-  squared_numbers = even_numbers.map do |num|
-    num ** 2
-  end 
+#   squared_numbers = even_numbers.map do |num|
+#     num ** 2
+#   end 
 
-  return squared_numbers
+#   return squared_numbers
+# end
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# resultado = process_numbers(numeros)
+
+# puts "Os quadrados dos números pares são: #{resultado}"
+
+# // Funções avançadas 02
+
+def calcular_media(notas)
+  total = notas.sum
+  media = total.to_f / notas.size
+  return media
 end
 
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-resultado = process_numbers(numeros)
-
-puts "Os quadrados dos números pares são: #{resultado}"
+def verificar_situacao(media)
+  if media >= 7.0
+    return "Aprovado"
+  elsif media >= 5.0
+    return "Recuperação"
+  else 
+    return "Reprovado"
+  end
+end
