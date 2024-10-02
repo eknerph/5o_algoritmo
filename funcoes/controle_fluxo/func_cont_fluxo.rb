337c133b01c0e78ff9c98a4b6b9dd5e77bb3071e
eknerph <eknerph@gmail.com>
2024-10-01 22:52:41 -0300

    Adicionado códigos de exemplo

--- funcoes/controle_fluxo/func_cont_fluxo.rb
+++ funcoes/controle_fluxo/func_cont_fluxo.rb
@@ -1,6 +1,9 @@
 # // Função com controle de Fluxo
 
 def maior_valor(num1, num2)
-  if num1, num2
-    return num1 > num2
-      return num1
+  if num1 > num2
+    return num1
+  else  
+    return num2
+  end
+end
