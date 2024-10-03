826d2e62d4ce2cd0a0fca87cfc6f9eed8e46b252
eknerph <eknerph@gmail.com>
2024-10-02 21:49:40 -0300

    Criado um novo arquivo para teste de controle de fluxo

---
+++ funcoes/controle_fluxo/func_cont_fluxo2.rb
@@ -0,0 +1,17 @@
+def classificar_idade(idade)
+  if idade < 12
+    return "Criança"
+  elsif idade >= 12 && idade <= 17
+    return "Adolecente"
+  elsif idade >= 18 && idade <= 64
+    return "Adulto"
+  else
+    return "Idoso"
+  end
+end
+idade1 = classificar_idade(10)
+idade2 = classificar_idade(15)
+idade3 = classificar_idade(30)
+
+
+
