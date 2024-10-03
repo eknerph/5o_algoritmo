dadc7081047c778059eb12030ad4baa3119056f3
eknerph <eknerph@gmail.com>
2024-10-02 22:04:55 -0300

    Adicionados os demais códigos para testes e teste rodou com sucesso

--- funcoes/controle_fluxo/func_cont_fluxo2.rb
+++ funcoes/controle_fluxo/func_cont_fluxo2.rb
@@ -4,9 +4,9 @@ def classificar_idade(idade)
   elsif idade >= 12 && idade <= 17
     return "Adolecente"
   elsif idade >= 18 && idade <= 64
-    return "Adulto"
+    return "Adulta"
   else
-    return "Idoso"
+    return "Idosa"
   end
 end
 idade1 = classificar_idade(10)
@@ -14,6 +14,17 @@ idade2 = classificar_idade(15)
 idade3 = classificar_idade(30)
 idade4 = classificar_idade(70)
 
+puts "Com 10 anos, a pessoa é: #{ idade1 }"
+puts "Com 15 anos, a pessoa é: #{ idade2 }"
+puts "Com 30 anos, a pessoa é: #{ idade3 }"
+puts "Com 70 anos, a pessoa é: #{ idade4 }"
 
+
+# // Funcionou este teste
+# Resposta
+# Com 10 anos, a pessoa é: Criança
+# Com 10 anos, a pessoa é: Adolecente
+# Com 10 anos, a pessoa é: Adulta
+# Com 10 anos, a pessoa é: Idosa
 
 
