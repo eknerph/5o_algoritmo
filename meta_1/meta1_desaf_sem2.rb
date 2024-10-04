73cee57ed71e259ffaa9bd78dec8d870026e789b
eknerph <eknerph@gmail.com>
2024-10-03 23:04:48 -0300

    Inseridos os códigos para testes meta_1/meta1_desaf_sem2.rb

--- meta_1/meta1_desaf_sem2.rb
+++ meta_1/meta1_desaf_sem2.rb
@@ -1,4 +1,11 @@
 puts "Desafio semana 02 percorrer array para encontrar maior número!"
 
 def encontra_numero(nums, alvo)
-  nums.each
+  nums.each_with_index do |num, i|
+    (i+...nums.length).each do |j|
+      if nums[1] + nums[j] == alvo
+        return [i, j]
+      end
+    end
+  end
+end
