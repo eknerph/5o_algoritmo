4ec1996a4b8a59f897791e7f32afcabbedb9f109
eknerph <eknerph@gmail.com>
2024-10-03 23:14:22 -0300

    Adicinado mais código de explicação para conclusão do exercicio

--- meta_1/array_maior_num.rb
+++ meta_1/array_maior_num.rb
@@ -1,12 +1,27 @@
-def maior_numero(lista)
-  maior = lista[0]
-  lista.each do |numero|
-    if numero > maior
-      maior = numero
-    end
-  end
-  maior
-end
+# def maior_numero(lista)
+#   maior = lista[0]
+#   lista.each do |numero|
+#     if numero > maior
+#       maior = numero
+#     end
+#   end
+#   maior
+# end
+
+# numeros = [10, 25, 7, 50, 32]
+# puts "O maior número é: #{ maior_numero(numeros) }"
+
+
+
+# // Outro exemplo para percorrer lista para número maior
 
 numeros = [10, 25, 7, 50, 32]
-puts "O maior número é: #{ maior_numero(numeros) }"
+maior = numeros[0]
+
+numeros.each do |numero|
+  if numero > maior
+    maior = numero
+  end
+end
+
+puts "O maior número é: #{maior}"
