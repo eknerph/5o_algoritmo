fe26c3a78705ec0666f2da620cc6a774a141cdeb
eknerph <eknerph@gmail.com>
2024-10-04 22:39:51 -0300

    Adicinado o código solicitado ndo def_sem2

--- meta_1/meta1_desaf_sem2.rb
+++ meta_1/meta1_desaf_sem2.rb
@@ -1,11 +1,16 @@
 puts "Desafio semana 02 percorrer array para encontrar maior número!"
 
-def encontra_numero(nums, alvo)
-  nums.each_with_index do |num, i|
-    (i+...nums.length).each do |j|
-      if nums[1] + nums[j] == alvo
-        return [i, j]
-      end
+def encontra_numero(nums)
+  maior = nums[0]
+  nums.each do |num|
+    if num > maior
+      maior = num
     end
   end
+
+  return maior
 end
+
+lista =[5, 12, 9, 3, 15, 7]
+resultado = encontra_numero(lista)
+puts "O maior valor é: #{ resultado }"
