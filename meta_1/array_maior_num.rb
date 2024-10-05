4c9849e3fd5cd7d0f96560833fb84e4b5440579f
eknerph <eknerph@gmail.com>
2024-10-04 22:58:54 -0300

    Adicinado outro exemplopar bucar por nome expecífico

--- meta_1/array_maior_num.rb
+++ meta_1/array_maior_num.rb
@@ -15,13 +15,33 @@
 
 # // Outro exemplo para percorrer lista para número maior
 
-numeros = [10, 25, 7, 50, 32]
-maior = numeros[0]
+# numeros = [10, 25, 7, 50, 32]
+# maior = numeros[0]
 
-numeros.each do |numero|
-  if numero > maior
-    maior = numero
+# numeros.each do |numero|
+#   if numero > maior
+#     maior = numero
+#   end
+# end
+
+# puts "O maior número é: #{maior}"
+
+
+# // Outro exemplo para percorrer na procura de nome expecífico
+
+nomes = ["João", "Fernanda", "Maria", "Carlos"]
+nome_procurado = "Fernada"
+encontrado = false
+
+nomes.each do |nome|
+  if nome == nome_procurado
+    encontrado = true
+    break
   end
 end
 
-puts "O maior número é: #{maior}"
+if encontrado
+  puts "O nome #{ nome_procurado } foi encontrado!"
+else
+  puts "O nome #{ nome_procurado } não foi encontrado"
+end
